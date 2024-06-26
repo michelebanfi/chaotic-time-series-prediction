9c4df2bf05ef61d0983b7db522b4eff8f59fafcf
michelebanfi <michi.banfi01@gmail.com>
2024-06-26 17:35:06 +0200

    animation

---
+++ RestrictedThreeBodyProblem/animate.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Circle
+import numpy as np
+
+# Load data from CSV
+df = pd.read_csv('Data/3BP.csv')
+data = df[['x', 'y', 'vx', 'vy']].values
+t = df['time'].values
+
+constants = pd.read_csv('Data/constants.csv')
+m1 = constants['mass'][0]
+m2 = constants['mass'][1]
+x1 = constants['x'][0]
+y1 = constants['y'][0]
+x2 = constants['x'][1]
+y2 = constants['y'][1]
+
+# create an animation and save it as .mp4 file
+
+fig, ax = plt.subplots()
+ax.set_xlim(-200, 200)
+ax.set_ylim(-200, 200)
+ax.set_aspect('equal')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Restricted Three-Body Problem')
+
+# plot the trajectory of the third body
+line, = ax.plot([], [], label='Trajectory of the third body')
+scat = ax.scatter([], [], color='blue', label='Cristiano Ronaldo', s=30)
+earth = Circle((x1, y1), 5, color='green', edgecolor='black', label='Earth')  # Adjusted size of Earth
+sun = Circle((x2, y2), 7, color='red', edgecolor='black', label='Udine')        # Adjusted size of Sun
+ax.add_patch(earth)
+ax.add_patch(sun)
+ax.legend()
+
+def init():
+    line.set_data([], [])
+    scat.set_offsets(np.array([[], []]).T)
+    return line, scat
+
+def animate(i):
+    line.set_data(data[:i, 0], data[:i, 1])
+    scat.set_offsets(data[i, :2])
+    return line, scat
+
+ani = animation.FuncAnimation(fig, animate, frames=len(t), init_func=init, blit=True)
+ani.save('Media/three_body_problem.mp4', writer='ffmpeg', fps=30, dpi=300)
+
--- Utils/DataEvaluator.py
+++ Utils/DataEvaluator.py
@@ -1,5 +1,8 @@
 import torch
 
+def NormalizedMeanSquaredError(y_true, y_pred):
+    return torch.mean((y_true - y_pred) ** 2) #/ torch.mean(y_true ** 2)
+
 def evaluate(num_epochs, criterion, optimizer, currentModel, train_dataloader, val_sequences_torch, val_targets_torch):
     losses = []
 
@@ -11,7 +14,7 @@ def evaluate(num_epochs, criterion, optimizer, currentModel, train_dataloader, v
             outputs = currentModel(inputs)
             targets = targets[-1, :, :]
             outputs = outputs.squeeze(0)
-            loss = criterion(outputs, targets)
+            loss = NormalizedMeanSquaredError(outputs, targets)
             loss.backward()
             optimizer.step()
 
@@ -25,9 +28,9 @@ def evaluate(num_epochs, criterion, optimizer, currentModel, train_dataloader, v
             val_predictions = torch.zeros(val_targets_torch.size(0), val_targets_torch.size(1), val_sequences_torch.size(2))
             for i in range(val_sequences_torch.size(0)):
                 val_predictions[i,:,:] = currentModel(val_sequences_torch[i, :, :].unsqueeze(0))
-            rmse = torch.sqrt(criterion(val_predictions, val_targets_torch))
+            #rmse = torch.sqrt(criterion(val_predictions, val_targets_torch))
             val_predictions_np = val_predictions.squeeze(0).numpy()
             val_target_np = val_targets_torch.squeeze(0).numpy()
-            print(f'RMSE: {rmse.item():.4f}')
+            #print(f'RMSE: {rmse.item():.4f}')
 
     return val_predictions_np, val_target_np, losses
